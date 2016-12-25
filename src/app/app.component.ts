bfcdd91cceaeca9e7b8ad3b78d990bdcda144d70
ysqi <devysq@gmail.com>
2016-12-25 21:52:47 +0800

    init atop project with Angular2 and Gentelella

---
+++ src/app/app.component.ts
@@ -0,0 +1,45 @@
+/*
+ * Angular 2 decorators and services
+ */
+import { Component, ViewEncapsulation } from '@angular/core';
+import { AppState } from './app.service';
+
+import 'bootstrap';
+import 'style-loader!css-loader!less-loader!font-awesome-webpack/font-awesome-styles.loader!font-awesome-webpack/font-awesome.config.js';
+import '../vendors/gentelella/custom.js';
+
+/*
+ * App Component
+ * Top Level Component
+ */
+@Component({
+  selector: 'app',
+  encapsulation: ViewEncapsulation.None,
+  styles: [
+    require('./app.style.less').toString()
+  ],
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  angularclassLogo = 'assets/img/angularclass-avatar.png';
+  name = 'Angular 2 Webpack Starter';
+  url = 'https://twitter.com/AngularClass';
+
+  constructor(
+    public appState: AppState) {
+
+  }
+
+  ngOnInit() {
+    console.log('Initial App State', this.appState.state);
+  }
+
+}
+
+/*
+ * Please review the https://github.com/AngularClass/angular2-examples/ repo for
+ * more angular app examples that you may copy/paste
+ * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
+ * For help or questions please contact us at @AngularClass on twitter
+ * or our chat on Slack at https://AngularClass.com/slack-join
+ */
